0be3b21fd65caa8217f036230a9133202cc310f2
saikiran7288 <115058076+saikiran7288@users.noreply.github.com>
2022-11-11 18:25:36 +0530

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. Why are functions advantageous to have in your programs?
+sol:
+i)function used to reduce the duplicate code.
+ii)This functions makes pograms shorter.easier to read  and also to update
+iii)its main is to code Reusablity2. When does the code in a function run: when it's specified or when it's called?
+
+sol:
+The code in a function executes when the function is called, not when the function is specified.3. What statement creates a function?
+sol:
+The def statement defines an function
+Syntax of function:
+def __function_name(parameters):
+__function body__
+return value4. What is the difference between a function and a function call?
+sol:
+a function is an procedure to achive an particular result.While function 
+call is to achive that Task.by using function in an program the function is called thats an cll to function perform certain given task.5. How many global scopes are there in a Python program? How many local scopes?
+sol:
+There is only one global and one local scope in python created  whenever a function is called.6. What happens to variables in a local scope when the function call returns?
+sol:
+when a function returns the local scope is destryed and all the variables in it are forgotten7. What is the concept of a return value? Is it possible to have a return value in an expression?
+sol:
+A return value is an function call evaluates to. like any value, a return  value can be used as part of an expression.8. If a function does not have a return statement, what is the return value of a call to that function?
+sol:
+if there is no return statement for an function the return value  of that function is None.
+9. How do you make a function variable refer to the global variable?
+sol:
+ A global statement will force a variable in a function to refer to the global variable.
+ If you want to refer to a global variable in a function, you can use the global keyword to declare which variables are global.10. What is the data type of None?
+sol:
+The datatype of None is NoneType.11. What does the sentence import areallyourpetsnamederic do?
+sol:
+ That import statement imports a module named areallyourpetsnamederic.12. If you had a bacon() feature in a spam module, what would you call it after importing spam?
+sol:
+Place the line of code that might cause an error in a try clause and use except block to handle the error.14. What is the purpose of the try clause? What is the purpose of the except clause?
+sol:
+when The code cause an error goes in the try clause. The code that executes if an error happens goes in the except clause.
